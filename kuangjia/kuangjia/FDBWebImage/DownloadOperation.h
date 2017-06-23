336991de079ef15ee771eb8c7b4174382368c5b6
duanyuanyuan <duanyuanyuanvip@163.com>
2017-06-23 16:08:56 +0800

    ..

---
+++ kuangjia/kuangjia/FDBWebImage/DownloadOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloadOperation.h
+//  kuangjia
+//
+//  Created by dyy on 2017/6/23.
+//  Copyright © 2017年 abc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloadOperation : NSOperation
+
+@end
